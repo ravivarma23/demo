03615a1a5e3e2455453278cc922003a6eb066f40
Balaji Dommaraju <balajidommaraju@SJC1-Lmac-N64741.home>
2021-03-17 15:53:43 -0500

    Added docker compose yaml file and test case

--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,8 +16,8 @@ public class DemoController {
     }
 
     @GetMapping("/hello")
-    public String sayHello(){
-        return "Hello Bootcamp";
+    public ResponseEntity<String> sayHello(){
+        return new ResponseEntity<String>("Hello Bootcamp",HttpStatus.OK);
     }
 
 }
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,28 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.mockito.InjectMocks;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@InjectMocks
+	DemoController demoController;
     @Test
-    void contextLoads() {
+    void testController(){
+    	MockHttpServletRequest request = new MockHttpServletRequest();
+        RequestContextHolder.setRequestAttributes(new ServletRequestAttributes(request));
+        
+        ResponseEntity<String> responseEntity = demoController.sayHello();
+        
+        assertEquals(responseEntity.getStatusCodeValue(),200);
+        
     }
 
 }
